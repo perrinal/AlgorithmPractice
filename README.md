cd2cff9fe64123766bf5784857168622622ca0d3
perrinal <alperrin@ALPERRIN-GB.uk.oracle.com>
2017-12-02 00:05:39 +0000

    deleted:    BinaryModeSearch.py 	deleted:    BinaryTreeNodeSearch.py 	modified:   README.md 	deleted:    RedBlackBinaryTree.py 	modified:   drawCircle.py

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+# Algorithm Practice
 
-These are my implementation of algorithmic practices
+These are my data structure and algorithm practices.
+Every file can be downloaded and executed directly from Python IDLE, I use only core libraries from the standard Python 3 distrbution so, no fancy library install required.
--- drawCircle.py
+++ drawCircle.py
@@ -1,60 +1,72 @@
+###################################################
+# Implementation of the Midpoint Circle algorithm #
+###################################################
+#
+# https://en.wikipedia.org/wiki/Midpoint_circle_algorithm
+# this is a very optimised way to draw a circle on screen (when the pixel resolution is not infinite)
+#
+# Click anywhere on the windows to draw a circle.
+
+
 from tkinter import *
 import time
+from time import sleep
 
-class DrawCircle:
-    def __init__(self):
-        pass 
-        
+
+root = Tk()
+root.title = "Game"
+root.resizable(0,0)
+root.wm_attributes("-topmost", 1)
+colours = ["#ffffff","#00ffff","#ff00ff","#ffff00","#0000ff","#ff0000","#00ff00"]
+canvas = Canvas(root, width=640, height=480, bd=0, highlightthickness=0, bg="#000000")
+canvas.pack()
+
+
+class Circle:
     
-##    def run(self):
-##        
-##        root = Tk()
-##
-##        self.entries = []
-##        row = Frame(root)
-##
-##        label = Label(row,text="Radius")
-##        label.config(font=('Ariel',10,'bold'))
-##        entry = Entry(row)
-##
-##        label.pack(side='left',padx=5)
-##        entry.pack(side='right',padx=5)
-##        row.pack()
-##        self.entries.append(entry)
-##        
-##        row = Frame(root)
-##        drawButton = Button(row,text="Draw",command=self.draw)
-##        drawButton.pack(side='left',padx=5)
-##
-##        row.pack(padx=5,pady=5)
+    def __init__(self, delay=0):
 
+        self.delay = delay
+        self.colour = 0
+        self.canvas = canvas
+        self.WIDTH, self.HEIGHT = 640, 480
+        self.CENTREX, self.CENTREY = int(self.WIDTH/2) , int(self.HEIGHT/2)
+
+        canvas.bind("<Button-1>", self.canvas_onclick)
+
+        self.img = PhotoImage(width=self.WIDTH, height=self.HEIGHT)
+        #self.draw()
+        self.img2 = canvas.create_image((self.WIDTH/2, self.HEIGHT/2), image=self.img, state="normal") 
+        mainloop()
+
+    def canvas_onclick(self, event):
+        self.draw()
+
+    def drawPoint(self,color , X,Y):
+        if X > 0 and Y > 0:
+            self.img.put(color, (X, Y))
+    
     def draw(self):
-        WIDTH, HEIGHT = 640, 480
-        CENTREX, CENTREY = int(WIDTH/2) , int(HEIGHT/2)
         radius = 100
-
-        window = Tk()
-        canvas = Canvas(window, width=WIDTH, height=HEIGHT, bg="#000000")
-        canvas.pack()
-        img = PhotoImage(width=WIDTH, height=HEIGHT)
-        canvas.create_image((WIDTH/2, HEIGHT/2), image=img, state="normal")
-
+        self.colour = (self.colour + 1 ) % len(colours)
+        centerX = root.winfo_pointerx() - root.winfo_rootx()
+        centerY = root.winfo_pointery() - root.winfo_rooty()
+        
         y = 0
         x = radius
         radiusError = 0
-        color = "#ffffff"
-        while y <= x:
+        
+                
+        while y <= x:      
+            self.drawPoint(colours[self.colour], centerX + x,centerY + y)
+            self.drawPoint(colours[self.colour], centerX + y,centerY + x)
+            self.drawPoint(colours[self.colour], centerX - x,centerY + y)
+            self.drawPoint(colours[self.colour], centerX - y,centerY + x)
+            self.drawPoint(colours[self.colour], centerX + x,centerY - y)
+            self.drawPoint(colours[self.colour], centerX + y,centerY - x)
+            self.drawPoint(colours[self.colour], centerX - x,centerY - y)
+            self.drawPoint(colours[self.colour], centerX - y,centerY - x)
             
-            img.put(color, (CENTREX + x,CENTREY + y))
-            img.put(color, (CENTREX + y,CENTREY + x))
-            img.put(color, (CENTREX - x,CENTREY + y))
-            img.put(color, (CENTREX - y,CENTREY + x))
-            img.put(color, (CENTREX + x,CENTREY - y))
-            img.put(color, (CENTREX + y,CENTREY - x))
-            img.put(color, (CENTREX - x,CENTREY - y))
-            img.put(color, (CENTREX - y,CENTREY - x))
-
-
             if radiusError <= 0:
                 y = y + 1
                 radiusError = radiusError + 2 * y + 1
@@ -62,11 +74,15 @@ class DrawCircle:
             if radiusError > 0:
                 x = x - 1
                 radiusError = radiusError - 2 * x - 1
-        mainloop()
+                
+            sleep(self.delay)
+            canvas.itemconfigure(self.img2, image=self.img)
+            root.update_idletasks()
+            root.update()
         
-            
-DC = DrawCircle()
-DC.draw()
+        
+# The delay help to visualise how circles are drawn            
+Crcl = Circle(0.001)
 
 
 
